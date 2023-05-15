1a9559800c1d644808ec7544800f3805dfdac1f8
Anupama Anilkumar <anupama@techgentsia.com>
2023-05-15 23:58:49 +0530

    refactor: counter and msg displaying component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
+
+     <div class="card">
+      <button type="button" @click="count = 0">Reset Counter</button>
+    </div>
+
+    <div class="card">
+      <input type="text" placeholder="Type something" v-model="dataEntered" />
+      <p>Typed text: {{ filteredValues }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    const count = ref(0);
+    const dataEntered = ref(" ");
+    const filteredValues = computed(() => {
+      return dataEntered ? dataEntered : "";
+    });
+
+    return {
+      count,
+      dataEntered,
+      filteredValues,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
